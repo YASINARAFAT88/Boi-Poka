2b2d48fe9ef5ba3ab44a68a205971d28813adcee
YASINARAFAT88 <yasin80545859@gmail.com>
2025-09-19 04:08:23 +0600

    added sort by

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,43 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Book = ({book}) => {
-    const {bookName, author, image, tags, category, bookId} = book
-    return (
-        <Link to={`books/${bookId}`}>
-        <div className="card bg-base-100 w-96  shadow-sm">
-  <figure>
-    <img
-      src={image} className='h-[166px]' />
-  </figure>
-  <div className="card-body">
-    <div className='flex justify-center gap-2'>
-        {
-            tags.map(tag => <button className="btn btn-xs">{tag}</button>)
-        }
-    </div>
-    <h2 className="card-title ">
-      {bookName}
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p className=''>By: {author}</p>
-    <div className='border-t-2 border-dashed mb-3'></div>
-    <div className="card-actions justify-between">
-      <div className="badge badge-outline">{category}</div>
-      <div>
-        <div className="rating">
-  <div className="mask mask-star" aria-label="1 star"></div>
-  <div className="mask mask-star" aria-label="2 star"></div>
-  <div className="mask mask-star" aria-label="3 star" aria-current="true"></div>
-  <div className="mask mask-star" aria-label="4 star"></div>
-  <div className="mask mask-star" aria-label="5 star"></div>
-</div>
-      </div>
-    </div>
-  </div>
+const Book = ({ book }) => {
+  const { bookName, author, image, tags, category, bookId, rating, totalPages } = book
+  return (
+    <Link to={`books/${bookId}`}>
+      <div className="card bg-base-100 w-96  shadow-sm">
+        <figure>
+          <img
+            src={image} className='h-[166px]' />
+        </figure>
+        <div className="card-body">
+          <div className='flex justify-center gap-2'>
+            {
+              tags.map(tag => <button className="btn btn-xs">{tag}</button>)
+            }
+          </div>
+          <h2 className="card-title ">
+            {bookName}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p className=''>By: {author}</p>
+          <div className='border-t-2 border-dashed mb-3'></div>
+          <div className="card-actions justify-between">
+            <div className="badge badge-outline">{category}</div>
+            <div>{rating}</div>
+            <div>{totalPages}</div>
+            <div>
+              <div className="rating">
+                <div className="mask mask-star" aria-label="1 star"></div>
+                <div className="mask mask-star" aria-label="2 star"></div>
+                <div className="mask mask-star" aria-label="3 star" aria-current="true"></div>
+                <div className="mask mask-star" aria-label="4 star"></div>
+                <div className="mask mask-star" aria-label="5 star"></div>
+              </div>
+            </div>
+          </div>
         </div>
-        </Link>
-    );
+      </div>
+    </Link>
+  );
 };
 
 export default Book;
--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -8,18 +8,38 @@ import Book from '../Book/Book';
 
 const ListedBooks = () => {
     const [readList, setReadList] = useState([])
+    const [sort, setSort] = useState('')
     const allBooks = useLoaderData();
 
-    useEffect(()=>{
+    useEffect(() => {
         const stredReadList = getStoredReadList();
         const stredReadListInt = stredReadList.map(id => parseInt(id))
         console.log(stredReadList, stredReadListInt, allBooks)
         const readBookList = allBooks.filter(book => stredReadListInt.includes(book.bookId))
         setReadList(readBookList)
-    },[])
+    }, [])
+
+    const handleSort = sortType =>{
+        setSort(sortType)
+        if(sortType === "No Of Pages"){
+            const sortedReadList = [...readList].sort((a, b) => a.totalPages - b.totalPages)
+            setReadList(sortedReadList)
+        }
+        if(sortType === "Ratings"){
+            const sortedReadList = [...readList].sort((a, b) => a.totalPages - b.totalPages)
+            setReadList(sortedReadList)
+        }
+    }
     return (
         <div>
             <h2 className='text-3xl my-4 font-bold'>Listed Books</h2>
+            <details className="dropdown">
+                <summary className="btn m-1">{sort ? `Sort By: ${sort}` : 'Sort By:'}</summary>
+                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                   <li onClick={()=> handleSort('Ratings')}><a>Ratings</a></li>
+                    <li onClick={()=> handleSort('No Of Pages')}><a>No Of Pages</a></li>
+                </ul>
+            </details>
             <Tabs className='w-3/4 my-4'>
                 <TabList>
                     <Tab>Read List</Tab>
@@ -30,7 +50,7 @@ const ListedBooks = () => {
                     <h2 className='text-2xl'>Books I Read: {readList.length}</h2>
                     {
                         readList.map(book => <Book
-                        key={book.bookId} book={book}
+                            key={book.bookId} book={book}
                         ></Book>)
                     }
                 </TabPanel>
